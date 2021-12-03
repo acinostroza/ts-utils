f4a12dfeb8555bad9a8c288e6d7c0bd766c214c9
Alvaro Cuadros-Inostroza <acuadros+github@gmail.com>
2021-12-03 12:28:28 +0100

    Improve download of Renviron.bioc
    Set the location of the environ file by means of a environ variable
    (or $HOME by default) and check for updates upstream using timestamps.

--- build-rdevel.sh
+++ build-rdevel.sh
@@ -12,21 +12,29 @@
 #   RDEST       path to where R devel will be installed (def: /tmp/R). The path
 #               is created automatically if needed.
 #   RVERSION    the R version to compile (def: R-devel)
+#   RENVBIOC    the location of the Renviron.bioc file (def: ~/.Renviron.bioc)
 #
 # Note: Be aware that the script will remove the current R installation
 #       in RDEST and build a fresh one.
 
 RDEST="${RDEST:-/tmp/R}"
 RVERSION="${RVERSION:-R-devel}"
+RENVBIOC="${RENVBIOC:-$HOME/.Renviron.bioc}"
+RENVURL="http://bioconductor.org/checkResults/devel/bioc-LATEST/Renviron.bioc"
+
+if [ -f "$RENVBIOC" ] ; then
+    curl -o "$RENVBIOC" -R -z "$RENVBIOC" "$RENVURL"
+else
+    curl -o "$RENVBIOC" -R "$RENVURL"
+fi
 
 mkdir -p "$RDEST" || exit 1
 cd "$RDEST" || exit 1
 
-test -f Renviron.bioc || wget http://bioconductor.org/checkResults/devel/bioc-LATEST/Renviron.bioc
 test -f "$RVERSION.tar.gz" && rm -r "$RVERSION.tar.gz"
 test -d "$RVERSION.tar.gz" && rm -rf "$RVERSION"
 
-wget "https://cran.r-project.org/src/base-prerelease/${RVERSION}.tar.gz" || exit 1
+curl -O "https://cran.r-project.org/src/base-prerelease/${RVERSION}.tar.gz" || exit 1
 
 tar xf "$RVERSION.tar.gz"
 cd "$RVERSION" && ./configure && make -j9
@@ -39,3 +47,5 @@ bin/R --vanilla <<EOF
    install.packages(c("ncdf4", "tinytest", "assertthat", "knitr", "BiocManager"))
    BiocManager::install(c("BiocStyle", "TargetSearchData"))
 EOF
+
+# vim: set ts=4 sw=4 et:
